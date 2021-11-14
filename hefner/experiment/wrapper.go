200980b1e418c52cb6198754883a2f4cb669987c
Daniel Owen van Dommelen <9572060+TheApeMachine@users.noreply.github.com>
2021-11-14 01:08:16 +0100

    Adding example for pipes

--- hefner/experiment/wrapper.go
+++ hefner/experiment/wrapper.go
@@ -8,6 +8,17 @@ func (pipe ProtoPipe) Wrap(modeller interface{}) chan Pipe {
 	return pipe.o
 }
 
+/*
+Your guess is as good as mine as to what this does. I wrote this a month or so back
+in what could be described as a haze...
+Apparently it is used somehow like this:
+
+dg.Unwrap(&artifact) <- <-dg.Generate()
+
+I suppose it will take a type that implements hefner Pipe and pushes its input
+through the output into the Unwrap channel with a type used to model the data
+into a structured type.
+*/
 func (pipe ProtoPipe) Unwrap(modeller interface{}) chan Pipe {
 	pipe.cache.Peek(pipe.key.String(), &modeller)
 	return pipe.o
