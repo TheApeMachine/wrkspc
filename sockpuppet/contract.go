dadea58fed77229b7a51e5ee48f6748f7c7fbf23
Daniel Owen van Dommelen <9572060+TheApeMachine@users.noreply.github.com>
2022-11-07 23:42:10 +0100

    cleanup of auth code

--- sockpuppet/contract.go
+++ sockpuppet/contract.go
@@ -8,14 +8,24 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
+/*
+Notary is an interface that manages data routing and validation
+from the FastHTTP server.
+*/
 type Notary interface {
 	Validate(string, *fasthttp.RequestCtx) (*big.Int, error)
 }
 
+/*
+Contract is a wrapper around a smart contract function.
+*/
 type Contract struct {
 	notary Notary
 }
 
+/*
+NewContract instantiates a pointer to a Contract.
+*/
 func NewContract(notary Notary) *Contract {
 	errnie.Traces()
 
@@ -24,14 +34,22 @@ func NewContract(notary Notary) *Contract {
 	}
 }
 
+/*
+Up brings up the smart contract function.
+*/
 func (contract *Contract) Up(port string) error {
 	return fasthttp.ListenAndServe(
 		":"+port, func(ctx *fasthttp.RequestCtx) {
+			// Call the Validate method on the Notary object, which
+			// manages the incoming request data and routes it to
+			// the smart contract logic.
 			code, err := contract.notary.Validate(string(ctx.Path()), ctx)
 
+			// Set the response headers on the connection context.
 			ctx.SetContentType("appplication/json")
 			ctx.SetStatusCode(int(code.Int64()))
 
+			// Write the response data back to the calling connection.
 			fmt.Fprint(ctx, err.Error())
 		},
 	)
--- sockpuppet/ethereum.go
+++ sockpuppet/ethereum.go
@@ -3,6 +3,7 @@ package sockpuppet
 import (
 	context "context"
 	"crypto/ecdsa"
+	"errors"
 	"fmt"
 	"math/big"
 
@@ -12,15 +13,22 @@ import (
 	"github.com/theapemachine/wrkspc/errnie"
 )
 
+/*
+EthClient is a wrapper around a client object that can
+communicate with the ethereum blockchain.
+*/
 type EthClient struct {
 	conn     *ethclient.Client
 	data     bind.ContractBackend
 	endpoint string
 	token    string
 	auth     *bind.TransactOpts
-	err      error
+	err      errnie.Error
 }
 
+/*
+NewEthClient instantiates a pointer to an Ethclient instance.
+*/
 func NewEthClient(endpoint, token string) *EthClient {
 	return &EthClient{
 		endpoint: endpoint,
@@ -28,49 +36,73 @@ func NewEthClient(endpoint, token string) *EthClient {
 	}
 }
 
+/*
+Dial to the ethereum blockchain.
+*/
 func (client *EthClient) Dial() *EthClient {
-	client.conn, client.err = ethclient.Dial(client.endpoint)
-	errnie.Handles(client.err)
-	client.auth = getAccountAuth(client.conn, client.token)
+	var err error
+	client.conn, err = ethclient.Dial(client.endpoint)
+	client.err = errnie.Handles(err)
+	client.auth, client.err = client.getAccountAuth(client.token)
 	return client
 }
 
+/*
+Conn returns the connection instance.
+*/
 func (client *EthClient) Conn() bind.ContractBackend {
 	return client.conn
 }
 
+/*
+Auth returns the authentication instance.
+*/
 func (client *EthClient) Auth() *bind.TransactOpts {
 	return client.auth
 }
 
-// function to create auth for any account from its private key
-func getAccountAuth(
-	client *ethclient.Client, privateKeyAddress string,
-) *bind.TransactOpts {
+/*
+getAccountAuth retrieves an authentication instance from a private key.
+*/
+func (client *EthClient) getAccountAuth(
+	privateKeyAddress string,
+) (*bind.TransactOpts, errnie.Error) {
 	privateKey, err := crypto.HexToECDSA(privateKeyAddress)
 	errnie.Handles(err)
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+
 	if !ok {
-		panic("invalid key")
+		return &bind.TransactOpts{}, errnie.Handles(
+			errnie.NewError(errors.New("invalid key")),
+		)
 	}
 
-	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	errnie.Handles(err)
+	var nonce uint64
+
+	if nonce, err = client.conn.PendingNonceAt(
+		context.Background(), crypto.PubkeyToAddress(*publicKeyECDSA),
+	); err != nil {
+		return &bind.TransactOpts{}, errnie.Handles(err)
+	}
 
 	fmt.Println("nounce=", nonce)
-	chainID, err := client.ChainID(context.Background())
+	chainID, err := client.conn.ChainID(context.Background())
 	errnie.Handles(err)
 
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
-	errnie.Handles(err)
+	var auth *bind.TransactOpts
+
+	if auth, err = bind.NewKeyedTransactorWithChainID(
+		privateKey, chainID,
+	); err != nil {
+		return &bind.TransactOpts{}, errnie.Handles(err)
+	}
 
 	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)      // in wei
-	auth.GasLimit = uint64(3000000) // in units
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = uint64(3000000)
 	auth.GasPrice = big.NewInt(1000000)
 
-	return auth
+	return auth, errnie.Error{}
 }
