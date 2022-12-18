57d0a63c60596b12b53c65b45c6d5db327ebe95e
Daniel Owen van Dommelen <theapemachine@gmail.com>
2022-12-18 16:13:51 +0100

    - Adds root command - Adds brazil package
    Signed-off-by: Daniel Owen van Dommelen <theapemachine@gmail.com>

---
+++ brazil/path.go
@@ -0,0 +1,76 @@
+package brazil
+
+import (
+	"os"
+	"strings"
+
+	"github.com/theapemachine/wrkspc/errnie"
+)
+
+/*
+Path wraps the structure of a filesystem to provide wrkspc with
+some specific functionality, and improving developer ergonomics.
+*/
+type Path struct {
+	Location string
+	err      error
+}
+
+/*
+NewPath takes a variatic input of path segments, which it will
+join with a slash (/) to make it behave inline as with most
+file system path.
+It will also create the path if it does not already exist.
+*/
+func NewPath(segments ...string) *Path {
+	path := &Path{}
+	path.Location = path.toPrefix(segments...)
+	return path
+}
+
+func ClearPath(path string) {
+	errnie.Handles(os.RemoveAll(path))
+}
+
+/*
+toPrefix takes the segments that were passed in and joins
+them into a traditional file system path shape.
+It is also cabable of handling file system location wildcards.
+*/
+func (path *Path) toPrefix(segments ...string) string {
+	var out string
+	var err error
+
+	switch segments[0] {
+	case "~":
+		out, err = os.UserHomeDir()
+	case ".":
+		out, err = os.Getwd()
+	default:
+		out = strings.Join(segments, "/")
+		path.makePath(out)
+	}
+
+	if errnie.Handles(err) != nil {
+		path.err = errnie.NewError(err)
+		return ""
+	}
+
+	errnie.Debugs("brazil.Path.toPrefix ->", out)
+	return out
+}
+
+/*
+makePath writes a new path on the file system if it was not
+already found.
+*/
+func (path *Path) makePath(prefix string) {
+	_, err := os.Stat(prefix)
+
+	if errnie.Handles(err) != nil {
+		path.err = errnie.NewError(err)
+		errnie.Handles(os.MkdirAll(prefix, os.ModePerm))
+		errnie.Informs("created new path at", prefix)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/theapemachine/wrkspc
+
+go 1.19
